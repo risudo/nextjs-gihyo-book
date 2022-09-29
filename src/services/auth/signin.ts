c94ad3cdd31ee6481c6f8a6ccbe59373a8434cdd
riosudo <badbad7351@gmail.com>
2022-09-29 09:43:24 +0900

    APIクライアント実装

---
+++ src/services/auth/signin.ts
@@ -0,0 +1,28 @@
+import { ApiContext, User } from "types";
+import { fetcher } from "utils";
+
+export type SigninParams = {
+  // サンプルユーザーのユーザー名は"user"
+  username: string;
+  // サンプルユーザーのユーザー名は"password"
+  password: string;
+};
+
+const signin = async (
+  context: ApiContext,
+  params: SigninParams
+): Promise<any> => {
+  return await fetcher(
+    `${context.apiRootUrl.replace(/\/$/g, "")}/auth/signin`,
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(params),
+    }
+  );
+};
+
+export default signin;
