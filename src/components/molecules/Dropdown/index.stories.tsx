f431a41b3c802344a1fbf075c1965683958a1c51
riosudo <badbad7351@gmail.com>
2022-10-06 11:24:23 +0900

    fix dropdown

--- src/components/molecules/Dropdown/index.stories.tsx
+++ src/components/molecules/Dropdown/index.stories.tsx
@@ -46,33 +46,33 @@ const Templete: ComponentStory<typeof Dropdown> = (args) => (
   <Dropdown {...args} />
 );
 
-export const Normal = Templete.bind({})
+export const Normal = Templete.bind({});
 Normal.args = {
   options: [
-    {value: null, label: '-'},
-    {value: 'one', label: 'One'},
-    {value: 'two', label: 'Two'},
-    {value: 'three', label: 'Three'},
+    { value: null, label: "-" },
+    { value: "one", label: "One" },
+    { value: "two", label: "Two" },
+    { value: "three", label: "Three" },
   ],
-  placeholder: 'Please select items from the list',
-}
+  placeholder: "Please select items from the list",
+};
 
-export const InitialValue = Templete.bind({})
+export const InitialValue = Templete.bind({});
 InitialValue.args = {
   options: [
-    {value: null, label: '-'},
-    {value: 'one', label: 'One'},
-    {value: 'two', label: 'Two'},
-    {value: 'three', label: 'Three'},
+    { value: null, label: "-" },
+    { value: "one", label: "One" },
+    { value: "two", label: "Two" },
+    { value: "three", label: "Three" },
   ],
-  placeholder: 'Please select items from the list',
-  value: 'one',
-}
+  placeholder: "Please select items from the list",
+  value: "one",
+};
 
-export const Many = Templete.bind({})
+export const Many = Templete.bind({});
 Many.args = {
   options: Array.from(Array(20), (_v, k) => {
-    return {value: k.toString(), label: k.toString()}
+    return { value: k.toString(), label: k.toString() };
   }),
-  placeholder: 'Please select items from the list',
-}
+  placeholder: "Please select items from the list",
+};
