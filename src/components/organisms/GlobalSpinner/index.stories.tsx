2d407dfe8bf9f6531bee248990640219b63b4db7
riosudo <badbad7351@gmail.com>
2022-10-11 09:52:54 +0900

    add GlobalSpinner

---
+++ src/components/organisms/GlobalSpinner/index.stories.tsx
@@ -0,0 +1,38 @@
+import { ComponentMeta } from "@storybook/react";
+import Button from "components/atoms/Button";
+import GlobalSpinnerContextProvider, {
+  useGlobalSpinnerActionContext,
+} from "contexts/GlobalSpinnerContext";
+import GlobalSpinner from ".";
+
+export default {
+  title: "organisms/GlobalSpinner",
+} as ComponentMeta<typeof GlobalSpinner>;
+
+export const WithContextProvider = () => {
+  const ChildComponent = () => {
+    const setGlobalSpinner = useGlobalSpinnerActionContext();
+    const handleClick = () => {
+      setGlobalSpinner(true);
+
+      // 5秒後に閉じる
+      setTimeout(() => {
+        setGlobalSpinner(false);
+      }, 5000);
+    };
+
+    return (
+      <>
+        <GlobalSpinner />
+        {/* クリックでスピナーを表示 */}
+        <Button onClick={handleClick}>スピナー表示</Button>
+      </>
+    );
+  };
+
+  return (
+    <GlobalSpinnerContextProvider>
+      <ChildComponent />
+    </GlobalSpinnerContextProvider>
+  );
+};
