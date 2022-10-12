dbc38e9be5e4c62daa6cd420ca0aa85d278af134
riosudo <badbad7351@gmail.com>
2022-10-12 11:15:18 +0900

    add ShoppingCartContext

--- src/contexts/ShoppingCartContext/index.tsx
+++ src/contexts/ShoppingCartContext/index.tsx
@@ -1,5 +1,6 @@
 import React, { useContext, useReducer } from "react";
 import { Product } from "types";
+import { ADD_PRODUCT, REMOVE_PRODUCT, shopReducer } from "./reducers";
 
 type ShoppingCartContextType = {
   cart: Product[];
@@ -24,4 +25,21 @@ export const ShoppingCartContextProvider = ({
   children,
 }: ShoppingCartContextProviderProps) => {
   const products: Product[] = [];
+  const [cartState, dispatch] = useReducer(shopReducer, products);
+
+  const addProductToCart = (product: Product) => {
+    dispatch({ type: ADD_PRODUCT, payload: product });
+  };
+
+  const removeProductFromCart = (productId: number) => {
+    dispatch({ type: REMOVE_PRODUCT, payload: productId });
+  };
+
+  return (
+    <ShoppingCartContext.Provider
+      value={{ cart: cartState, addProductToCart, removeProductFromCart }}
+    >
+      {children}
+    </ShoppingCartContext.Provider>
+  );
 };
