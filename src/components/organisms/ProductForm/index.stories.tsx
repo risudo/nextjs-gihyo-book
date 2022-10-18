9b8042e65ec5b85935505ad8dda3164c5994d2d1
riosudo <badbad7351@gmail.com>
2022-10-18 13:24:15 +0900

    add ProductCard

---
+++ src/components/organisms/ProductForm/index.stories.tsx
@@ -0,0 +1,19 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import ProductForm from ".";
+
+export default {
+  title: "Organisms/ProductForm",
+  argTypes: {
+    onProductSave: {
+      description: "出品ボタンを押した時のイベントハンドラ",
+      table: {
+        type: { summary: "function" },
+      },
+    },
+  },
+} as ComponentMeta<typeof ProductForm>;
+
+const Templete: ComponentStory<typeof ProductForm> = (args) => (
+  <ProductForm {...args}/>
+)
+export const Form = Templete.bind({})
